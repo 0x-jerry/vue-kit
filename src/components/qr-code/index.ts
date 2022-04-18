6ccef5f7ff46c5f6905fd3f9052f89bab754b1ae
Jerry Wang <x.jerry.wang@gmail.com>
2022-04-18 20:43:37 +0800

    feat: QR Code component

---
+++ src/components/qr-code/index.ts
@@ -0,0 +1,1 @@
+export { default as KQrCode } from './KQrCode.vue'
---
+++ src/docs/004TestQrCode.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+import TestBox from './components/TestBox.vue'
+import { KQrCode } from '@/components/qr-code'
+
+const data = reactive({})
+</script>
+
+<template>
+  <test-box title="QR Code">
+    <k-qr-code content="1234"></k-qr-code>
+  </test-box>
+</template>
