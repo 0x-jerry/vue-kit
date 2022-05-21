d800c6c810a8fedcfd43be5c24932e0b1db0f99d
Jerry Wang <x.jerry.wang@gmail.com>
2022-05-21 10:31:15 +0800

    feat: implement switch component

--- src/docs/012Switch.vue
+++ src/docs/012Switch.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import TestBox from './components/TestBox.vue'
+import KRow from '@/components/layout/KRow.vue'
 
 const data = reactive({
   v: false,
@@ -8,9 +9,15 @@ const data = reactive({
 
 <template>
   <test-box title="Switch">
-    <k-switch :modelValue="false"></k-switch>
-    <k-switch :modelValue="true"></k-switch>
-    <k-switch v-model="data.v"></k-switch>
-    <k-switch :modelValue="data.v" disabled></k-switch>
+    <k-row>
+      <span> false: </span>
+      <k-switch :modelValue="false"></k-switch>
+      <span> true: </span>
+      <k-switch :modelValue="true"></k-switch>
+      <span> interactive: </span>
+      <k-switch v-model="data.v"></k-switch>
+      <span> disabled: </span>
+      <k-switch :modelValue="data.v" disabled></k-switch>
+    </k-row>
   </test-box>
 </template>
