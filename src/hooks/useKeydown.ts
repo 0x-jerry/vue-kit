e877817011d84c4786b517001d2d187aec231d8c
Jerry Wang <x.jerry.wang@gmail.com>
2022-11-19 19:58:43 +0800

    fix: typedef

--- src/hooks/useKeydown.ts
+++ src/hooks/useKeydown.ts
@@ -1,5 +1,6 @@
 import { createKeyboardEventHandler } from '@/functions'
+import { Fn } from '@vueuse/core'
 
-export function useGlobalKeydown(key: string, listener: (e: KeyboardEvent) => any) {
+export function useGlobalKeydown(key: string, listener: (e: KeyboardEvent) => any): Fn {
   return useEventListener('keydown', (e) => createKeyboardEventHandler(key, () => listener(e))(e))
 }
