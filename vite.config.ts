ba847b2b5d0b746956dd630dd1352234ca05eeab
Jerry <x.jerry.wang@gmail.com>
2025-01-15 09:08:51 +0000

    fix: build component

---
+++ vite.config.ts
@@ -0,0 +1,39 @@
+/// <reference types="vitest/config" />
+import { defineConfig } from 'vite'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import vue from '@vitejs/plugin-vue'
+import { readdir } from 'node:fs/promises'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+  plugins: [vue(), vueJsx(), dts({ insertTypesEntry: true })],
+  build: {
+    lib: {
+      entry: await getEntries(),
+      formats: ['es'],
+    },
+    rollupOptions: {
+      external: ['vue', '@vueuse/core'],
+    },
+  },
+  // @ts-ignore
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
+})
+
+async function getEntries() {
+  const entries: Record<string, string> = {
+    index: 'src/lib.ts',
+  }
+
+  const componentDir = 'src/components'
+  const files = await readdir(componentDir)
+
+  for (const name of files) {
+    entries[`components/${name}`] = `${componentDir}/${name}/index.ts`
+  }
+
+  return entries
+}
