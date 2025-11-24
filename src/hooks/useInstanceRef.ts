0dd2b86f8f6bdb95bee2aa66ffcafc1b97aff887
Jerry <x.jerry.wang@gmail.com>
2025-11-24 07:41:17 +0000

    perf(useInstanceRef): use shallowRef for better performance

--- src/hooks/useInstanceRef.ts
+++ src/hooks/useInstanceRef.ts
@@ -1,6 +1,6 @@
-import { ref } from 'vue'
+import { shallowRef } from 'vue'
 import type { ComponentExposed } from 'vue-component-type-helpers'
 
 export function useInstanceRef<T>(component: T) {
-  return ref<ComponentExposed<T>>()
+  return shallowRef<ComponentExposed<T>>()
 }
