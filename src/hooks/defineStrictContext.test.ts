a58c9932abe1307cd7e590004efa5e43ae344f56
Jerry <x.jerry.wang@gmail.com>
2026-01-15 01:53:20 +0000

    feat: defineStrictContext

---
+++ src/hooks/defineStrictContext.test.ts
@@ -0,0 +1,74 @@
+import { mount } from '@vue/test-utils'
+import { defineComponent, type Ref, reactive, ref } from 'vue'
+import { defineStrictContext } from './defineStrictContext'
+
+describe('defineContext', () => {
+  it('test with parameters', async () => {
+    const useCount = defineStrictContext(
+      'key',
+      (opt: { count: Ref<number> }) => {
+        const state = reactive({
+          count: opt.count,
+          add() {
+            opt.count.value++
+          },
+        })
+
+        return state
+      },
+    )
+
+    const Child = defineComponent({
+      setup() {
+        const p = useCount()
+
+        return {
+          p,
+        }
+      },
+      template: `<button @click="p.add()">{{p.count}}</button>`,
+    })
+
+    const App = defineComponent({
+      components: { B: Child },
+      setup() {
+        const p = useCount.provide({
+          count: ref(0),
+        })
+
+        return {
+          p,
+        }
+      },
+      template: `<div :count="p.count"><B /></div>`,
+    })
+
+    const wrapper = mount(App)
+
+    expect(wrapper.attributes()?.['count']).toBe('0')
+    expect(wrapper.get('button').text()).toBe('0')
+
+    await wrapper.get('button').trigger('click')
+    expect(wrapper.attributes()?.['count']).toBe('1')
+    expect(wrapper.get('button').text()).toBe('1')
+  })
+
+  it('should throw error when missing context key', () => {
+    const useCount = defineStrictContext('key', () => {})
+
+    const Child = defineComponent({
+      setup() {
+        useCount()
+      },
+      template: `<button>1</button>`,
+    })
+
+    const App = defineComponent({
+      components: { B: Child },
+      setup() {},
+      template: `<div><B /></div>`,
+    })
+
+    expect(() => mount(App)).to.throw('Can not find context key: key')
+  })
+})
---
+++ src/hooks/defineStrictContext.ts
@@ -0,0 +1,56 @@
+import type { Fn } from '@0x-jerry/utils'
+import { type InjectionKey, inject, provide } from 'vue'
+
+export interface DefineStricutContext<Options extends unknown[], Context> {
+  (): Context
+  (defaultValue: Context): Context
+
+  provide(...arg: Options): Context
+}
+
+export function defineStrictContext<T extends {}>(
+  key: InjectionKey<T>,
+): DefineStricutContext<[T], T>
+// biome-ignore lint/suspicious/noExplicitAny: hack types
+export function defineStrictContext<F extends (...args: any[]) => unknown>(
+  key: string | symbol | InjectionKey<ReturnType<F>>,
+  factory: F,
+): DefineStricutContext<Parameters<F>, ReturnType<F>>
+
+// implementation
+export function defineStrictContext(
+  key: string | symbol | InjectionKey<unknown>,
+  factory?: Fn,
+) {
+  const useContext = (defaultContext: unknown) => {
+    const ctx = inject(key, defaultContext)
+
+    if (!ctx) {
+      throw new Error(`Can not find context key: ${key.toString()}`)
+    }
+
+    return ctx
+  }
+
+  useContext.provide = getProviderFn()
+
+  return useContext
+
+  function getProviderFn() {
+    if (typeof factory === 'function') {
+      return (...args: unknown[]) => {
+        const ctx = factory(...args)
+
+        provide(key, ctx)
+
+        return ctx
+      }
+    }
+
+    return (value: unknown) => {
+      provide(key, value)
+
+      return value
+    }
+  }
+}
